cd4d1b91286253409d649cea4b1a72ea9baac855
HamedKh99 <goo.hamedacc@gmail.com>
2020-02-26 00:43:32 +0330

    debugged homepage

--- homepage/models.py
+++ homepage/models.py
@@ -6,11 +6,11 @@ class Heading(models.Model):
     title = models.CharField(max_length=100)
     motto = models.CharField(max_length=200)
 
-    def save(self, force_insert=False, force_update=False, using=None,
-             update_fields=None):
-        if Heading.objects.exists():
-            raise ValidationError('Heading Already Exists!')
-        return super(Heading, self).save(force_insert, force_update, using, update_fields)
+    # def save(self, force_insert=False, force_update=False, using=None,
+    #          update_fields=None):
+    #     if Heading.objects.exists():
+    #         raise ValidationError('Heading Already Exists!')
+    #     return super(Heading, self).save(force_insert, force_update, using, update_fields)
 
 
 class Section(models.Model):
@@ -24,11 +24,11 @@ class ContactUs(models.Model):
     email = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=100)
 
-    def save(self, force_insert=False, force_update=False, using=None,
-             update_fields=None):
-        if ContactUs.objects.exists():
-            raise ValidationError('ContactUs Already Exists!')
-        return super(ContactUs, self).save(force_insert, force_update, using, update_fields)
+    # def save(self, force_insert=False, force_update=False, using=None,
+    #          update_fields=None):
+    #     if ContactUs.objects.exists():
+    #         raise ValidationError('ContactUs Already Exists!')
+    #     return super(ContactUs, self).save(force_insert, force_update, using, update_fields)
 
 
 class SocialNetwork(models.Model):
