009eebaf1f39b364dd35c8d326929feec369ddc7
HamedKh99 <goo.hamedacc@gmail.com>
2020-03-27 21:59:54 +0430

    added export for haftkhan

--- Borna/settings.py
+++ Borna/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'homepage',
-    'events'
+    'events',
+    'import_export'
 ]
 
 MIDDLEWARE = [
@@ -125,5 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+
+class HaftkhanSignupResource(resources.ModelResource):
+    class Meta:
+        model = HaftkhanSignup
+
+
+class HaftkhanSignupAdmin(ImportExportModelAdmin):
+    resource_class = HaftkhanSignupResource
+
+
+class StudentResource(resources.ModelResource):
+    class Meta:
+        model = Student
+
+
+class StudentAdmin(ImportExportModelAdmin):
+    resource_class = StudentResource
+
 
 admin.site.register(HaftkhanPre)
-admin.site.register(HaftkhanSignup)
-admin.site.register(Student)
+admin.site.register(HaftkhanSignup, HaftkhanSignupAdmin)
+admin.site.register(Student, StudentAdmin)
